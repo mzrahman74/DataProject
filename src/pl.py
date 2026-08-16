19c2ad395c7aecc0b9404ac4e525b4647962290f
Mohammad Rahman <mzrahman74@gmail.com>
2026-08-16 14:09:02 -0500

    added hist and line kind

--- src/pl.py
+++ src/pl.py
@@ -5,3 +5,10 @@ df = pd.read_csv('../data/data.csv')
 
 df.plot(kind='scatter', x='Duration', y='Calories')
 plt.show()
+
+
+df.plot(kind='line', x='Duration', y='Maxpulse')
+plt.show()
+
+df["Duration"].plot(kind='hist')
+plt.show()
